6f510486428774992817d2a534eacb73e476643c
hrqmartins <hrqmartins129@gmail.com>
2025-11-13 11:17:04 -0300

    feat: Função para estabelecer conexão com o banco de dados Oracle.

--- symbio-admin.py
+++ symbio-admin.py
@@ -15,3 +15,30 @@ import requests
 
 # URL da API de IA 
 API_IA_URL = "https://symbio-api-ia.onrender.com/"
+
+def getConexao():
+    '''
+    Estabelece e retorna uma conexão com o banco de dados Oracle.
+    '''
+    try:
+        # Tenta estabelecer a conexão
+        conn = oracledb.connect(
+            user="XXXXXX",
+            password="XXXXX",
+            host="oracle.fiap.com.br",
+            port=1521,
+            service_name="ORCL"
+        )
+        return conn
+    except oracledb.Error as e:
+        # Captura e exibe erros de conexão
+        print(
+        f"""[ERRO]: Não foi possível conectar ao Oracle: {e}\n
+Verifique se:
+1. O Oracle está instalado no sistema.
+2. As credenciais (user, password) estão corretas."""
+        )
+        return None
+    except Exception as e:
+        print(f"\n[ERRO]: Ocorreu um erro: {e}")
+        return None
