e320351b69ad3b82b36562e262382787e4700939
hrqmartins <hrqmartins129@gmail.com>
2025-11-13 11:08:31 -0300

    feat: Módulos necessários e descrição do projeto comentado.

---
+++ symbio-admin.py
@@ -0,0 +1,14 @@
+'''
+Global Solution - Computational Thinking Using Python
+1TDSPF - Henrique Martins - RM563620
+1TDSPF - Henrique Teixeira - RM563620
+
+[SYMBIO] Sistema Administrativo.
+Ferramenta administrativa para gestão de Cargos e Skills no banco de dados Oracle.
+Inclui integração via API REST para cálculo de risco de automação com IA.
+'''
+
+# Importação dos Módulos
+import oracledb
+import os
+import requests 
