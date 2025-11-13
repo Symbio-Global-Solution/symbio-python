1ecb4fe27d11578abdefb9bb74d5d329344e7c72
hrqmartins <hrqmartins129@gmail.com>
2025-11-13 11:35:22 -0300

    feat: Função para limpar o terminal e exibir titulo personalizado em ASCII ART feito no FSymbols

--- symbio-admin.py
+++ symbio-admin.py
@@ -16,6 +16,27 @@ import requests
 # URL da API de IA 
 API_IA_URL = "https://symbio-api-ia.onrender.com/"
 
+# Funções para Estilização do Terminal
+def limpar_terminal():
+    '''
+    Limpa o terminal.
+    '''
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def titulo_sistema():
+    print('-=≡≣ ------------------ X -------------------- ≣≡=-')
+    print('''
+
+░██████╗██╗░░░██╗███╗░░░███╗██████╗░██╗░█████╗░
+██╔════╝╚██╗░██╔╝████╗░████║██╔══██╗██║██╔══██╗
+╚█████╗░░╚████╔╝░██╔████╔██║██████╦╝██║██║░░██║
+░╚═══██╗░░╚██╔╝░░██║╚██╔╝██║██╔══██╗██║██║░░██║
+██████╔╝░░░██║░░░██║░╚═╝░██║██████╦╝██║╚█████╔╝
+╚═════╝░░░░╚═╝░░░╚═╝░░░░░╚═╝╚═════╝░╚═╝░╚════╝░
+''')
+    print('-=≡≣ ------------------ X -------------------- ≣≡=-')
+
+# Função para o Banco de Dados
 def getConexao():
     '''
     Estabelece e retorna uma conexão com o banco de dados Oracle.
@@ -42,3 +63,31 @@ Verifique se:
     except Exception as e:
         print(f"\n[ERRO]: Ocorreu um erro: {e}")
         return None
+    
+# Funções de Gestão de Cargos 
+def obter_risco_ia(features: list):
+    '''
+    Chama a API de IA para obter a predição de risco.
+    '''
+    try:
+        url = f"{API_IA_URL}/prever/risco"
+        # O JSON que a API espera: {"features": [num1, num2, num3]}
+        payload = {"features": features}
+        
+        response = requests.post(url, json=payload, timeout=5)
+        
+        if response.status_code == 200:
+            return response.json().get("risco_predito")
+        else:
+            print(f"[ERRO]: A API de IA teve um erro {response.status_code}: {response.text}")
+            return None
+            
+    except requests.exceptions.ConnectionError:
+        print("\n[ERRO DE REDE] Não foi possível conectar à API de IA (Flask).")
+        print("Verifique se o servidor 'symbio-app.py' está rodando.")
+        return None
+    except Exception as e:
+        print(f"\n[ERRO INESPERADO API]: {e}")
+        return None
+    
+titulo_sistema()
